2488ccfb1d9e7e3f38f1afc5362627d07c456120
Remi <remi.arosio@gmail.com>
2017-12-29 17:43:30 +0100

    We can now have a number of spell

--- app/src/main/java/com/app/remi/test/activities/MainActivity.java
+++ app/src/main/java/com/app/remi/test/activities/MainActivity.java
@@ -19,11 +19,12 @@ public class MainActivity extends Activity {
         Intent intent = getIntent();
 
         //Check is the sensorbutton is on or off
+        int number = intent.getIntExtra("SPELL_BLOCKS_NUMBER",3),
         Boolean playWithSensor = intent.getBooleanExtra("BOOLEAN_CHECK", false);
         SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
 
-        moteur = new Moteur(this, playWithSensor, sensorManager);
+        moteur = new Moteur(this, playWithSensor, sensorManager,number);
         setContentView(moteur);
 
 
