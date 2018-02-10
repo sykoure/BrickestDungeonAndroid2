ec528e7712f316bb7bbaab9aa1db1b6e70cbc1dd
kalahel <mathieu95480@gmail.com>
2018-02-10 13:59:41 +0100

    Added Junit

---
+++ app/src/test/java/com/app/remi/test/data/SpellBlockBuilderTest.java
@@ -0,0 +1,35 @@
+package com.app.remi.test.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Mat on 10/02/2018.
+ */
+public class SpellBlockBuilderTest {
+    @Test
+    public void spell() throws Exception {
+    }
+
+    @Test
+    public void dimension() throws Exception {
+    }
+
+    @Test
+    public void cooldown() throws Exception {
+    }
+
+    @Test
+    public void cooldownDuration() throws Exception {
+    }
+
+    @Test
+    public void position() throws Exception {
+    }
+
+    @Test
+    public void build() throws Exception {
+    }
+
+}
