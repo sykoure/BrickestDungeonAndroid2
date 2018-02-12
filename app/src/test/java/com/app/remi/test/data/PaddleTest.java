d1b3a4109279d6dd33a5edc42fe27cdc9e351a5b
Remi <remi.arosio@gmail.com>
2018-02-12 07:27:06 +0100

    The test classes are now created

---
+++ app/src/test/java/com/app/remi/test/data/PaddleTest.java
@@ -0,0 +1,63 @@
+package com.app.remi.test.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Remi on 12/02/2018.
+ */
+public class PaddleTest {
+    @Test
+    public void getRect() throws Exception {
+    }
+
+    @Test
+    public void getX() throws Exception {
+    }
+
+    @Test
+    public void getLength() throws Exception {
+    }
+
+    @Test
+    public void getHeight() throws Exception {
+    }
+
+    @Test
+    public void setSpeedSensor() throws Exception {
+    }
+
+    @Test
+    public void setLength() throws Exception {
+    }
+
+    @Test
+    public void getPaddleMoving() throws Exception {
+    }
+
+    @Test
+    public void setPaddleMoving() throws Exception {
+    }
+
+    @Test
+    public void setX() throws Exception {
+    }
+
+    @Test
+    public void setY() throws Exception {
+    }
+
+    @Test
+    public void getY() throws Exception {
+    }
+
+    @Test
+    public void getSpeed() throws Exception {
+    }
+
+    @Test
+    public void setSpeed() throws Exception {
+    }
+
+}
