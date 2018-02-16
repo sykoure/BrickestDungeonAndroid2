d5236fd4caca6b972fa9f5f5cdc992403a7e3ed7
Remi <remi.arosio@gmail.com>
2018-02-16 14:47:10 +0100

    Some tests are done

--- app/src/test/java/com/app/remi/test/data/SpellBlockBuilderTest.java
+++ app/src/test/java/com/app/remi/test/data/SpellBlockBuilderTest.java
@@ -6,8 +6,19 @@ import static org.junit.Assert.*;
 
 /**
  * Created by Mat on 10/02/2018.
+ * This class will test the spellBlock object and its builder
  */
 public class SpellBlockBuilderTest {
+
+    // The spellBlock object
+    private SpellBlock spellBlock = new SpellBlock.SpellBlockBuilder(0,0).
+                                        spell("spell").
+                                        dimension(0,0).
+                                        position(0,0).
+                                        cooldownDuration(5).
+                                        cooldown(0).
+                                        build();
+
     @Test
     public void spell() throws Exception {
     }
